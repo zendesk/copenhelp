877d379a8a3d2425e2c9cbbc29add16a08550db0
Andre Alves <andrenpalves@gmail.com>
2017-01-25 18:19:12 +0100

    Read data from static structure

--- shared/js/lib/query.js
+++ shared/js/lib/query.js
@@ -1,36 +1,53 @@
 var Facility = require('cloud/models/facility');
+var data = require('./data');
 
 var findByFilter = function(params) {
   var deferred = $.Deferred();
+  var result = data;
+  var offset = result[0];
+  var facilities = result.slice(1);
+  var category = params.filter.categories[0];
 
-  Parse.Cloud.run('findByFilter', params, {
-    success: function(result) {
-      deferred.resolve({
-        data: result.slice(1),
-        offset: result[0]
-      });
-    },
+  var filteredFacilities = [];
 
-    error: function(err) {
-      deferred.reject(err);
+  facilities.forEach(function(data) {
+    if(hasCategory(data,category)) {
+      filteredFacilities.push(convertFacility(data));
     }
+  });
+
+  deferred.resolve({
+    data: filteredFacilities,
+    offset: offset
   });
 
   return deferred.promise();
 };
 
+var hasCategory = function(data, category) {
+  var result = data.services.find(function(service) {
+    return service.category === category;
+  });
+  return !!result;
+};
+
+var convertFacility = function(data) {
+  var facility = new Facility(data);
+  facility.attributes.services = data.services;
+  return facility;
+};
+
 var findById = function(id) {
   var deferred = $.Deferred();
+  var result = data;
 
-  Parse.Cloud.run('findById', {id: id}, {
-    success: function(result) {
-      deferred.resolve(result);
-    },
+  function isElem(elem) {
+    return elem.objectId == id;
+  }
 
-    error: function(err) {
-      deferred.reject(err);
-    }
-  });
+  var found = result.find(isElem);
+  var facility = convertFacility(found);
+  deferred.resolve(facility);
 
   return deferred.promise();
 };
@@ -39,3 +56,4 @@ module.exports = {
   findByFilter: findByFilter,
   findById: findById
 };
+
